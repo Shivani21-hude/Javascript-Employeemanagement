c59a1f544cf087a5d472a9935d3916fd9b3632dd
Shivani21-hude <shivani21hude@gmail.com>
2023-04-19 18:32:17 +0530

    [Shivani]-Find Total employee wage

---
+++ EmployeeManagement/EmpWage.js
@@ -0,0 +1,23 @@
+let FullTime=1;
+let PartTime=2;
+let empHrs=0;
+let WagePerHour=20;
+let empCheck=Math.floor(Math.random()*10)%2;
+switch(empCheck)
+{
+case FullTime:
+    empHrs=8;
+    console.log("Employee is Full Time ")
+    break;
+    
+case PartTime:
+    empHrs=4;
+    console.log("Employee is Part Time")
+    break;
+
+default:
+    empHrs=0;
+    break;
+}
+let Total_Wage=empHrs*WagePerHour;
+console.log("Total employee wage is : "+Total_Wage);
