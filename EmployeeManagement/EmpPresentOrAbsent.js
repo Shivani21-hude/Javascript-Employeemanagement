110dc4ee9b599eb21c2cd8e599ece62801be427f
Shivani21-hude <shivani21hude@gmail.com>
2023-04-19 17:58:22 +0530

    [Shivani]-Check employee is present or absent

---
+++ EmployeeManagement/EmpPresentOrAbsent.js
@@ -0,0 +1,10 @@
+let Is_Absent=0;
+let empCheck=Math.floor(Math.random()*10)%2;
+if(empCheck==Is_Absent)
+{
+   console.log("Employee is absent");
+}
+else
+{
+   console.log("Employee is present");
+}
